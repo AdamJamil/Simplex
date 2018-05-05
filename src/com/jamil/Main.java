84600b4001495a80133e19cf35eb3f6b4e89bb4f
HerpPotato <adam.a1d9c@gmail.com>
2018-05-05 12:08:12 -0400

    completed mixed integer programming

--- src/com/jamil/Main.java
+++ src/com/jamil/Main.java
@@ -9,106 +9,12 @@ public class Main
 
     Main()
     {
-        double[][] vals = new double[][]{{3, 0, 4}, {2, 1, 1}, {3, 4, 1}};
-        double[] b = new double[]{10, 7, 12};
-        double[] c = new double[]{4, 5, 3};
-        boolean[] ineq = new boolean[]{true, true, true};
-        boolean[] integral = new boolean[]{true, false, true};
+        double[] c = new double[]{9, 5, 6, 4};
+        double[][] vals = new double[][]{{6, 3, 5, 2}, {0, 0, 1, 1}, {-1, 0, 1, 0}, {0, -1, 0, 1}, {0, -1, 0, 0}, {-1, 0, 0, 0}};
+        double[] b = new double[]{10, 1, 0, 0, -1, -1};
+        boolean[] ineq = new boolean[]{true, true, true, true, true, true};
+        boolean[] integral = new boolean[]{false, false, false, false};
         Tableau tableau = new Tableau(c.length, vals, b, c, ineq, integral);
         tableau.solve();
     }
-
-    /*void dualSimplex(double[][] arr)
-    {
-        int col = arr[0].length;
-        int row = arr.length;
-
-        //select pivot row
-        double min = 9999999;
-        int pivotRow = -1;
-        for (int i = 0; i < row - 1; i++)
-            if (arr[i][col - 1] < min)
-            {
-                min = arr[row - 1][i];
-                pivotRow = i;
-            }
-
-        //select pivot column
-        double max = -999999;
-        int pivotCol = -1;
-        for (int i = 0; i < col - 1; i++)
-            if (arr[pivotRow][i] < 0)
-                if (arr[row - 1][i] / arr[pivotRow][i] > max)
-                {
-                    max = arr[row - 1][i] / arr[pivotRow][i];
-                    pivotCol = i;
-                }
-
-        //normalize pivot row
-        double pivot = arr[pivotRow][pivotCol];
-        for (int i = 0; i < col; i++)
-            arr[pivotRow][i] /= pivot;
-
-        //set all other entries in pivot col to 0
-        for (int i = 0; i < row; i++)
-        {
-            //obviously we don't want to do this to the pivot row itself
-            if (i == pivotRow)
-                continue;
-
-            //select the row multiple
-            double mult = arr[i][pivotCol];
-
-            //subtract rows
-            for (int j = 0; j < col; j++)
-                arr[i][j] -= mult * arr[pivotRow][j];
-        }
-    }*/
-
-    /*void simplex(double[][] arr)
-    {
-        int col = arr[0].length;
-        int row = arr.length;
-
-        //select pivot column
-        double min = 9999999;
-        int pivotCol = -1;
-        for (int i = 0; i < col - 1; i++)
-            if (arr[row - 1][i] < min)
-            {
-                min = arr[row - 1][i];
-                pivotCol = i;
-            }
-
-        //select pivot row
-        min = 9999999;
-        int pivotRow = -1;
-        for (int i = 0; i < row - 1; i++)
-            if (arr[i][pivotCol] > 0 && arr[i][col - 1] > 0)
-                if (arr[i][col - 1] / arr[i][pivotCol] < min)
-                {
-                    min = arr[i][col - 1] / arr[i][pivotCol];
-                    pivotRow = i;
-                }
-
-        //normalize pivot row
-        double pivot = arr[pivotRow][pivotCol];
-        for (int i = 0; i < col; i++)
-            arr[pivotRow][i] /= pivot;
-
-        //set all other entries in pivot col to 0
-        for (int i = 0; i < row; i++)
-        {
-            //obviously we don't want to do this to the pivot row itself
-            if (i == pivotRow)
-                continue;
-
-            //select the row multiple
-            double mult = arr[i][pivotCol];
-
-            //subtract rows
-            for (int j = 0; j < col; j++)
-                arr[i][j] -= mult * arr[pivotRow][j];
-        }
-    }*/
 }
